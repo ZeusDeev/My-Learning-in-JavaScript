909965f4a6f6c3551510a467d711070f48538f1b
ZeusDeev <zeus.cach@outlook.com>
2025-03-31 20:11:15 -0500

    Destructuring, aprendiendo su aplicacion

---
+++ destructuring.js
@@ -0,0 +1,20 @@
+// Destructuracion en JavaScript
+
+const numeros = [1,2,3]
+
+const [uno, dos, tres] = numeros
+
+console.log(uno, dos, tres)
+
+// Destructuracion con objetos
+
+const persona = {
+    nombre: "Zeus",
+    apellido: "Cach",
+    edad: 21
+}
+
+const {nombre, apellido, edad} = persona
+console.log(nombre, apellido, edad)
+
+
